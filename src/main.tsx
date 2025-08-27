db93ea0c5882310036cc7d6a7b8dca690fea2bd9
lokendra-singh-dhirawat <lokendra20048@gmail.com>
2025-08-27 16:03:42 +0000

    set-up main file and set-up tanstack querry

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import "./index.css";
+import { createHashRouter, RouterProvider } from "react-router";
+import ReactDom from "react-dom/client";
+import routes from "./routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "./context/authContext";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+const router = createHashRouter(routes);
+
+ReactDom.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
